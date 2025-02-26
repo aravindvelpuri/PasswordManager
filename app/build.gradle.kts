621c7664ae218ef1a3ce1c8f8173e04710814602
Aravind Projects <projects.aravind@gmail.com>
2025-02-26 14:40:52 +0530

    Bugs fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aravindprojects.passwordmanager"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
